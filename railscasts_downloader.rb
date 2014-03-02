e03aca27d00dd52a07e525ae8f149d9df28f6efa
Ted Cheng <yiqun.cheng@gmail.com>
2014-03-01 16:41:49 -0800

    use better formating for output

--- railscasts_downloader.rb
+++ railscasts_downloader.rb
@@ -2,7 +2,7 @@
 require 'rss'
 require 'debugger'
 
-p 'Downloading rss index'
+puts 'Downloading rss index'
 
 rss_string = open('http://feeds.feedburner.com/railscasts').read
 rss = RSS::Parser.parse(rss_string, false)
@@ -11,7 +11,7 @@ videos_urls = rss.items.map { |it| it.enclosure.url }.reverse
 videos_filenames = videos_urls.map {|url| url.split('/').last }
 existing_filenames = Dir.glob('*.mp4')
 missing_filenames = videos_filenames - existing_filenames
-p "Downloading #{missing_filenames.size} missing videos"
+puts "Downloading #{missing_filenames.size} missing videos"
 
 missing_videos_urls = videos_urls.select { |video_url| missing_filenames.any? { |filename| video_url.match filename } }
 
@@ -19,8 +19,8 @@ missing_videos_urls = videos_urls.select { |video_url| missing_filenames.any? { 
 
 missing_videos_urls.each do |video_url|
   filename = video_url.split('/').last
-  p filename
-  p %x(curl -C - #{video_url} -o #{filename}.tmp)
-  p %x(mv #{filename}.tmp #{filename})
+  puts filename
+  %x(curl -C - #{video_url} -o #{filename}.tmp)
+  %x(mv #{filename}.tmp #{filename})
 end
-p 'Finished synchronization'
+puts 'Finished synchronization'
